913fcffd1bbe51cf161d0cad19e7202cc5bd4996
AlainBox <93340821+AlainBox@users.noreply.github.com>
2021-12-25 14:23:20 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Helo Winc Academy Students")
+// This is a large sum.
+/*
+let sum = 1230941 * 1823794
+console.log("sum:", sum)
+let sum_two = 637263 / 54
+console.log("sum_two:", sum_two)
+*/
+console.log("The comment won't show up in Node, because it's not a line of code.")
+console.log("The results are hidden when you write an block comment, because it prevents Node from executing the code.")
